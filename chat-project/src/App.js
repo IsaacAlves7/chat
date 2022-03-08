08efafdb64e915614b4dde42bb05e03115125b1c
IsaacAlves7 <isaac.alves0720@gmail.com>
2022-03-08 16:39:27 -0300

    Some removed files

--- chat-project/src/App.js
+++ chat-project/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
+    // step 1: removed logo.svg, App.test.js, setupTests.js
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's build a MERN Whatsapp clone</h1>
     </div>
   );
 }
