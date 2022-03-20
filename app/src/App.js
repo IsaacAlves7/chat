c10bbc6333c302ea522565f38da3e410282e971c
Isaac Alves Pinheiro <61624336+IsaacAlves7@users.noreply.github.com>
2022-03-20 13:41:26 -0300

    8th commit - AUTOGLASS CHAT

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,17 @@
 import './index.css';
+import Messenger from './components/Messenger';
+// import {WhatsappMessenger} from './components/Messenger'; importando mais de uma função no mesmo arquivo
 
 function App() {
-  return (
-    <div>
+  return ( // Toda função de return(), dentro dela, necessita de uma Tag HTML5 para iniciar o overrides de importação (<></> Tag vazia = recomendado)
+   <div>
       {/* <p>Hello, World!</p> */}
+      <Messenger/> {/* Aqui ela está chamando a função de importação para dentro da função do App */}
+      {/*<WhatsappMessenger/>*/}
     </div>
   );
 }
-
+// Aqui ela está exportando a função App, para outros arquivos acessarem pela importação 
+// Somente um export default é permitido por módulo
+// Para você exportar mais de uma função, é necessário usar o export na própria função: export const função () => {}
 export default App;
---
+++ app/src/components/Messenger.jsx
@@ -0,0 +1,17 @@
+const Messenger = () => {
+    return (
+        <>
+         <p>Hello from Messenger</p>
+        </>
+    )
+}
+
+// export const WhatsappMessenger = () => {
+//     return (
+//         <>
+//          <p>Hello from WhatsappMessenger</p>
+//         </>
+//     )
+// }
+
+export default Messenger;
