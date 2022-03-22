d56c02e27e8f6cea3db5cc7f79520396f080b2f0
Isaac Alves Pinheiro <61624336+IsaacAlves7@users.noreply.github.com>
2022-03-22 17:38:18 -0300

    19th commit - AUTOGLASS CHAT (49%)

--- server/database/db.js
+++ server/database/db.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 // Função assíncrona
 const Connection = async (username, password) => { 
+    // <username>:<password><databasename>
     const URL = `mongodb+srv://${username}:${password}@cluster0.94nsy.mongodb.net/autoglasschat?retryWrites=true&w=majority`;
     try { // Tentar e esperar por uma promise do mongoose connection with MongoDB
         await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
