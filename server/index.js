5003dd9768e44a5a204a6348647b5103801dac03
Isaac Alves Pinheiro <61624336+IsaacAlves7@users.noreply.github.com>
2022-03-22 15:57:51 -0300

    16th commit - AUTOGLASS CHAT (46%)

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 // IMPORTING...
 import express from "express";
 import Connection from "./database/db.js";
+import dotenv from "dotenv";
 
 // APP CONFIG
 const app = express();
 
 // DATABASE CONNECTION
 Connection();
+dotenv.config();
 
 // MIDDLEWARE
 
 // API ROUTES
 
 // PORT
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
 app.listen(PORT, () => console.log(`The server app is running successfully on http://localhost:${PORT}`));
 
 // TEST
